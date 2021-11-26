c3d6a3e326b1f9f649e44693453e48b4b8a47dc3
Christian Döring <doering.christian.2001@gmx.de>
2021-11-26 15:37:25 +0100

    Initial commit.

---
+++ build/CMakeFiles/clauncher.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clauncher.dir/src/main.c.o"
+  "CMakeFiles/clauncher.dir/src/main.c.o.d"
+  "clauncher"
+  "clauncher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/clauncher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
